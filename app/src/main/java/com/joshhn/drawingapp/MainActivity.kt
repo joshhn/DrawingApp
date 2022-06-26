a2d780cbe815cadad4c6eec09fe123f489b34e68
joshhn <duy.huynhngoc.05@gmail.com>
2022-06-26 16:04:11 -0400

    Add Canvas and Brush

---
+++ app/src/main/java/com/joshhn/drawingapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.joshhn.drawingapp
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        drawingView = findViewById(R.id.drawingView)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+
+
+
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn : ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
+    }
+}
